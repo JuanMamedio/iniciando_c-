3318eea09590c2c9140306cb07452548e07d141b
Juan Mamedio <140915478+JuanMamedio@users.noreply.github.com>
2024-02-13 15:23:25 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -20,13 +20,63 @@ Console.WriteLine("Seu nome é " + nome + "_" + sobrenome +"!");
 
 /*Exemplo 2*/
 
-string? nome, sobrenome;
 
+/*
+string? nome, sobrenome;
 Console.WriteLine("Escreva seu primeiro nome");
 nome = Console.ReadLine();
 Console.WriteLine("Escreva seu sobre nome");
 sobrenome = Console.ReadLine();
 
-string? nomecompleto = nome + sobrenome;
+string? nomecompleto = nome +" " + sobrenome;
 
 Console.WriteLine("Seja bem vindo caro " + nomecompleto);
+*/
+
+/*Desafio 1
+
+int B1,A1;
+
+Console.WriteLine("Primeiro digite a Base");
+B1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite a altura");
+A1 = int.Parse(Console.ReadLine());
+
+ double area = (B1 * A1);
+
+Console.WriteLine("A area é " + area);
+
+*/
+
+/*Desafio 2
+int nota1,nota2, nota3, nota4;
+
+Console.WriteLine("Digite a primeira nota");
+nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota");
+nota2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a terceira nota");
+nota3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a quarta nota");
+nota4 = int.Parse(Console.ReadLine());
+
+ double media = (nota1 + nota2 + nota3 + nota4)/4;
+
+Console.WriteLine("A sua nota média é " + media);
+*/
+
+/*Desafio 3
+
+string? nome, idade;
+
+Console.WriteLine("Escreva seu primeiro nome");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade");
+idade = Console.ReadLine();
+
+Console.WriteLine("Olá " + nome + " você tem " + idade + " anos de idade");
+*/
