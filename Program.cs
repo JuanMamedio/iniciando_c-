1151cc521cf471de490b7543a792183218e06baa
Juan Mamedio <140915478+JuanMamedio@users.noreply.github.com>
2024-02-12 23:37:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+// int numero = 12; 
+// float numero-com-ponto = 12.4
+// double numerocomponto = 12,2d
+// string texto = 'Definindo tipo texto'
+// bool boleano = false;
+
+/*Exemplo 1*/
+
+/*
+string? nome, sobrenome;
+
+Console.WriteLine("Escreva seu primeiro nome");
+nome = Console.ReadLine();
+Console.WriteLine("Escreva seu sobre nome");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine("Seu nome é " + nome + "_" + sobrenome +"!");
+*/
+
+/*Exemplo 2*/
+
+string? nome, sobrenome;
+
+Console.WriteLine("Escreva seu primeiro nome");
+nome = Console.ReadLine();
+Console.WriteLine("Escreva seu sobre nome");
+sobrenome = Console.ReadLine();
+
+string? nomecompleto = nome + sobrenome;
+
+Console.WriteLine("Seja bem vindo caro " + nomecompleto);
